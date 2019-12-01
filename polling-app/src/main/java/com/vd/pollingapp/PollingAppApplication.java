e2aa7e1e1f88df51ca607d142cf614e093466fc9
Vivek S Deshmukh <vsdrocks467@gmail.com>
2019-12-01 20:02:48 +0530

    initial commit for polling app

---
+++ polling-app/src/main/java/com/vd/pollingapp/PollingAppApplication.java
@@ -0,0 +1,13 @@
+package com.vd.pollingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingAppApplication.class, args);
+	}
+
+}
